61444345008bebfda1842807429ba47ce0e752fa
yanqirenshi <yanqirenshi@gmail.com>
2021-11-09 21:21:35 +0900

    add dev/

---
+++ dev/src/lib/components/cards/Message.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+/** ****************************************************************
+ *   props = {
+ *     data: {
+ *       message: {
+ *         title: string,
+ *         contents: string,
+ *       },
+ *     },
+ *     masonry_item: {
+ *       column_width: number,
+ *       item_name:    string,
+ *     },
+ *   }
+ * **************************************************************** */
+export default function Message (props) {
+    const data = props.source.data;
+    const masonry_item = props.source.masonry_item;
+
+    const col = data.size.col || 1;
+
+    const style = {
+        width:  masonry_item.column_width * col,
+        height: 'auto', // masonry_item.column_width * row,
+        borderRadius: 8,
+        marginBottom: 22,
+        fontSize: 14,
+        head: {
+            padding: '11px 22px',
+        },
+        body: {
+            padding: 11,
+            wordBreak: 'break-word',
+        },
+    };
+
+    return (
+        <div className={masonry_item.item_name}
+             style={style}>
+
+          <article className="message is-danger">
+
+            <div className="message-header" style={style.head}>
+              <p>{data.message.title}</p>
+            </div>
+
+            <div className="message-body" style={style.body}>
+              {data.message.contents}
+            </div>
+
+          </article>
+
+        </div>
+    );
+}
---
+++ dev/src/lib/index.js
@@ -0,0 +1,17 @@
+import Cockpit from './components/Cockpit.js';
+
+import Link       from './components/cards/Link.js';
+import Message    from './components/cards/Message.js';
+import SmallLarge from './components/cards/SmallLarge.js';
+import Plane      from './components/cards/Plane.js';
+
+import Core from './js/Core.js';
+
+export default Cockpit;
+export {
+    Core,
+    Link,
+    Message,
+    SmallLarge,
+    Plane,
+};
